b5707bce7fc8407e3f183c13275e3f1908ac3f4a
pezosanta <pezosanta@gmail.com>
2019-03-15 20:22:21 +0100

    dataset v2

--- dataset.py
+++ dataset.py
@@ -1,10 +1,13 @@
 from PIL import Image
+from skimage import io
+from skimage import color
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torchvision
 import torchvision.transforms.functional as F
 import glob
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 ########### Creating train, val, test datapaths ###########
@@ -50,24 +53,33 @@ class Bdd100k_dataset(Dataset):
     def __init__(self, image_paths, target_paths, train=True):
         self.image_paths = image_paths
         self.target_paths = target_paths
+        self.device = torch.device('cuda:0')
 
     def transform(self, image, mask):
         
         ### Resize, if ResNet requires it ###
-        # resize = transforms.Resize(size=(520, 520))
-        # image = resize(image)
-        # mask = resize(mask)
+        #resize = torchvision.transforms.Resize(size=(520, 520))
+        #imagei = resize(image)
+        #maski = resize(mask)
+
+        ### RGBA => RGB Conversion if neccessary ###
+        mask_rgb = mask
+        if mask.shape == (720, 1280, 4):
+            mask_rgb = color.rgba2rgb(mask)
       
-        ### Transform PIL Image to tensor ###
-        image = F.to_tensor(image)
-        mask = F.to_tensor(mask)
-        
-        return image, mask
+        ### Transform Numpy Array to tensor ###
+        tensor_image = torch.tensor(image, dtype = torch.float)
+        tensor_mask = torch.tensor(mask_rgb, dtype = torch.float)
+                
+        return tensor_image, tensor_mask
 
     def __getitem__(self, index):
-        image = Image.open(self.image_paths[index])
-        mask = Image.open(self.target_paths[index])
+        image = io.imread(self.image_paths[index])
+        mask = io.imread(self.target_paths[index])
         x, y = self.transform(image, mask)
+        
+        #return {'image':x, 'mask':y}
+                
         return x, y
 
     def __len__(self):
@@ -75,9 +87,10 @@ class Bdd100k_dataset(Dataset):
 
 
 
-########### Creating the DataLoaders ###########
+
 if __name__ == "__main__":
 
+    ########### Creating the DataLoaders ###########
     train_dataset = Bdd100k_dataset(train_image_paths, train_mask_paths)
     #print(len(train_dataset))
     train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=1)
@@ -90,27 +103,46 @@ if __name__ == "__main__":
     #print(len(test_dataset))
     test_loader = DataLoader(test_dataset, batch_size=4, shuffle=True, num_workers=1)
 
+    ########### Checking the Dimensions ###########
+    #for i in range(len(train_dataset)):
+    #    sample = train_dataset[i]
+
+    #    print(i, sample['image'].shape, sample['mask'].shape)
+
+
+    ########### Testing DataLoaders ###########        
+    images, masks = next(iter(train_loader))
+
+    #out = torchvision.utils.make_grid(images)  
     
-
- ########### Testing DataLoaders ###########       
+    print(images[0].shape, masks.shape)
     
-    images, masks = next(iter(train_loader))  # Some kind of tensor dimension problem..
+    DLTNIT = plt.figure('DataLoader Tensor => Numpy Image test')            # Something is wrong with the plotted pictures
+    image_numpy = images[0].numpy()    
+    print('DataLoader Tensor => Numpy Image test: ', image_numpy.shape)
+    plt.imshow(image_numpy)
+    DLTNIT.show()
+
+    DLTNMT = plt.figure('DataLoader Tensor => Numpy Mask test')
+    mask_numpy = masks[0].numpy()
+    print('DataLoader Tensor => Numpy Mask test: ', mask_numpy.shape)
+    plt.imshow(mask_numpy)
+    DLTNMT.show()
     
-    out = torchvision.utils.make_grid(images)
+    
+    ########### Testing RGBA => RGB Conversion ###########
+    MTBC = plt.figure('Mask test before RGBA => RGB conversion')
+    mask_testing = io.imread(train_mask_paths[0])                   # RGBA mask image!
+    print('Mask test before RGBA => RGB conversion: ', mask_testing.shape)
+    plt.imshow(mask_testing)
+    MTBC.show()
 
-    ### Testing if the PIL Image -> tensor conversion is the problem.... Seems good to me ###
-    image_testing = Image.open(train_image_paths[0])
-    image_testing.show()
-    mask_testing = Image.open(train_mask_paths[0])
-    mask_testing.show()
-
-    image_testing_to_tensor = F.to_tensor(image_testing)
-    mask_testing_to_tensor = F.to_tensor(mask_testing)
-
-    image_tensor_to_numpy = image_testing_to_tensor.numpy().transpose((1, 2, 0))
-    figure = plt.imshow(image_tensor_to_numpy)
-    plt.show()
-
-    mask_tensor_to_numpy = mask_testing_to_tensor.numpy().transpose((1, 2, 0))
-    figure = plt.imshow(mask_tensor_to_numpy)
+    MTAC = plt.figure('Mask test after RGBA => RGB conversion')
+    mask_testing_rgb = mask_testing
+    if mask_testing.shape == (720, 1280, 4):                        # Conversion with scikit-image
+            mask_testing_rgb = color.rgba2rgb(mask_testing)
+    print('Mask test after RGBA => RGB conversion: ', mask_testing_rgb.shape)
+    plt.imshow(mask_testing_rgb)
+    MTAC.show()
+    
     plt.show()
