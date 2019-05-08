4d9ceedf974cdcd43731f4ee18ec98fdbe101ef3
pezosanta <pezosanta@gmail.com>
2019-05-08 22:24:45 +0200

    feature_extractor v3

--- feature_extractor.py
+++ feature_extractor.py
@@ -90,11 +90,13 @@ class Bottleneck(nn.Module):
                     
         out += residual
         out = self.relu(out)
-    
+
+        '''
         print('resnetBlock')
         print(out.shape)
         print('downsample_connections')
         print(residual.shape)
+        '''
 
         return out
 
@@ -144,30 +146,30 @@ class ResNet(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
 
-        print('resnet_conv')
-        print(x.shape)
+        #print('resnet_conv')
+        #print(x.shape)
 
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
 
-        print('resnet_avgpool')
-        print(x.shape)
+        #print('resnet_avgpool')
+        #print(x.shape)
 
-        print('MEMORY ALLOCATED')
-        print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
+        #print('MEMORY ALLOCATED')
+        #print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
         x = self.layer1(x)
-        print('MEMORY ALLOCATED')
-        print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
+        #print('MEMORY ALLOCATED')
+        #print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
         x = self.layer2(x)
-        print('MEMORY ALLOCATED')
-        print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
+        #print('MEMORY ALLOCATED')
+        #print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
         x_3 = self.layer3(x)
-        print('MEMORY ALLOCATED')
-        print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
+        #print('MEMORY ALLOCATED')
+        #print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
         x = self.layer4(x_3)
-        print('MEMORY ALLOCATED')
-        print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
+        #print('MEMORY ALLOCATED')
+        #print(torch.cuda.memory_allocated(torch.device('cuda:0'))/(1024**3))
 
         #x = self.avgpool(x)
         #x = x.view(x.size(0), -1)
